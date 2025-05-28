394fed2a2750dfc75d4aa8b771f3350dc17568cb
RaphaelLandim <raphael.landim@yahoo.com.br>
2025-05-27 23:27:36 -0300

    Update pje.py

--- pje.py
+++ pje.py
@@ -36,5 +36,9 @@ def contato():
 
 # ... demais rotas com current_page adequado
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
